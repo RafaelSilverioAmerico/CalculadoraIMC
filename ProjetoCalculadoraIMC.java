b6498895fa71393c5d35237c2a9fc369160dae0f
Rafael Americo <rafael_silverio@outlook.com>
2025-10-23 09:12:18 -0300

    Codigo Funcionando

--- ProjetoCalculadoraIMC.java
+++ ProjetoCalculadoraIMC.java
@@ -32,5 +32,20 @@ public class ProjetoCalculadoraIMC extends Application{
 		double imc = peso / (altura * altura);
 		etiquetaResultado.setText(String.format("Seu IMC é: %.2f,", imc));
 	});
+
+	// Leayout na vertical
+    VBox layout = new VBox(10, etiquetaPeso, campoPeso, etiquetaAltura, campoAltura, botaoCalcular, etiquetaResultado);
+    layout.setPadding(new Insets(10));
+    layout.setAlignment(Pos.CENTER);
+
+    // Cena e palco
+    Scene cena = new Scene(layout, 300, 250);
+    palco.setTitle("Calculadora de IMC");
+    palco.setScene(cena);
+    palco.show();
 	}
+
+  public static void main(String[] args){
+	launch(args);
+   }	
 }
